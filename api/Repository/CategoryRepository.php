14afca79f7a44cb4b8758be9c95023e394278d3f
GilianC <163392501+GilianC@users.noreply.github.com>
2024-10-21 10:28:08 +0200

    Update CategoryRepository.php

--- api/Repository/CategoryRepository.php
+++ api/Repository/CategoryRepository.php
@@ -23,23 +23,18 @@ class CategoryRepository extends EntityRepository {
         parent::__construct();
     }
 
-    public function find($id): ?Category{
-        /*
-            La façon de faire une requête SQL ci-dessous est "meilleur" que celle vue
-            au précédent semestre (cnx->query). Notamment l'utilisation de bindParam
-            permet de vérifier que la valeur transmise est "safe" et de se prémunir
-            d'injection SQL.
-        */
-        $requete = $this->cnx->prepare("select * from Category where id=:value"); // prepare la requête SQL
-        $requete->bindParam(':value', $id); // fait le lien entre le "tag" :value et la valeur de $id
+    public function find($id_category): ?Category {
+        $requete = $this->cnx->prepare("select * FROM `Category` WHERE id_category= :value"); // prepare la requête SQL
+        $requete->bindParam(':value', $id_category); // fait le lien entre le "tag" :value et la valeur de $id
         $requete->execute(); // execute la requête
         $answer = $requete->fetch(PDO::FETCH_OBJ);
         
-        if ($answer==false) return null; // may be false if the sql request failed (wrong $id value for example)
+        if ($answer == false) return null; // may be false if the sql request failed (wrong $id value for example)
         
-        $p = new Category($answer->id);
-        $p->setName($answer->name);
-        return $p;
+        $c = new Category($answer->id_category);
+        $c->setLibelleCate($answer->libelle_cate);
+
+        return $c;
     }
 
     public function findAll(): array {
@@ -49,28 +44,41 @@ class CategoryRepository extends EntityRepository {
 
         $res = [];
         foreach($answer as $obj){
-            $p = new Category($obj->id);
-            $p->setName($obj->name);
-            array_push($res, $p);
+            $c = new Category($obj->id_category);
+            $c->setLibelleCate($obj->libelle_cate);
+
+            array_push($res, $c);
         }
        
         return $res;
     }
+   
+    // public function save($product){
+    //     $requete = $this->cnx->prepare("insert into Product (name, category) values (:name, :idcategory)");
+    //     $name = $product->getName();
+    //     $idcat = $product->getIdcategory();
+    //     $requete->bindParam(':name', $name );
+    //     $requete->bindParam(':idcategory', $idcat);
+    //     $answer = $requete->execute(); // an insert query returns true or false. $answer is a boolean.
 
-    public function save($product){
+    //     if ($answer){
+    //         $id = $this->cnx->lastInsertId(); // retrieve the id of the last insert query
+    //         $product->setId($id); // set the product id to its real value.
+    //         return true;
+    //     }
           
-        return false;
-    }
+    //     return false;
+    // }
 
-    public function delete($id){
-        // Not implemented ! TODO when needed !
-        return false;
-    }
+    // public function delete($id){
+    //     // Not implemented ! TODO when needed !
+    //     return false;
+    // }
 
-    public function update($product){
-        // Not implemented ! TODO when needed !
-        return false;
-    }
+    // public function update($product){
+    //     // Not implemented ! TODO when needed !
+    //     return false;
+    // }
 
    
     
