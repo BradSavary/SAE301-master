d6f9f2b1cd42b3e0dc9e0f53d916ef98c4c49c94
GilianC <163392501+GilianC@users.noreply.github.com>
2024-10-21 10:26:09 +0200

    Update Category.php

--- api/Class/Category.php
+++ api/Class/Category.php
@@ -1,43 +1,47 @@
 <?php
 /**
- *  Class Product
+ *  Class Category
  * 
  *  Représente un produit avec uniquement 3 propriétés (id, name, category)
  * 
  *  Implémente l'interface JsonSerializable 
  *  qui oblige à définir une méthode jsonSerialize. Cette méthode permet de dire comment les objets
- *  de la classe Product doivent être converti en JSON. Voire la méthode pour plus de détails.
+ *  de la classe Category doivent être converti en JSON. Voire la méthode pour plus de détails.
  */
 class Category implements JsonSerializable {
-    private int $id; // id du produit
-    private string $name; // nom du produit
+    private int $id_category; // id du produit
+    private string $libelle_cate; // nom du produit
 
-    public function __construct(int $id){
-        $this->id = $id;
+ 
+    public function __construct(int $id_category){
+
+        $this->id_category = $id_category;
+
+
     }
 
     /**
      * Get the value of id
      */ 
-    public function getId(): int
+    public function getIdcategory(): int
     {
-        return $this->id;
+        return $this->id_category;
     }
 
     /**
-     *  Define how to convert/serialize a Product to a JSON format
-     *  This method will be automatically invoked by json_encode when apply to a Product
+     *  Define how to convert/serialize a Category to a JSON format
+     *  This method will be automatically invoked by json_encode when apply to a Category
      * 
-     *  En français : On sait qu'on aura besoin de convertir des Product en JSON pour les
+     *  En français : On sait qu'on aura besoin de convertir des Category en JSON pour les
      *  envoyer au client. La fonction json_encode sait comment convertir en JSON des données
      *  de type élémentaire. A savoir : des chaînes de caractères, des nombres, des booléens
      *  des tableaux ou des objets standards (stdClass). 
-     *  Mais json_encode ne saura pas convertir un objet de type Product dont les propriétés sont
+     *  Mais json_encode ne saura pas convertir un objet de type Category dont les propriétés sont
      *  privées de surcroit. Sauf si on définit la méthode JsonSerialize qui doit retourner une
-     *  représentation d'un Product dans un format que json_encode sait convertir (ici un tableau associatif)
+     *  représentation d'un Category dans un format que json_encode sait convertir (ici un tableau associatif)
      * 
-     *  Le fait que Product "implémente" l'interface JsonSerializable oblige à définir la méthode
-     *  JsonSerialize et permet à json_encode de savoir comment convertir un Product en JSON.
+     *  Le fait que Category "implémente" l'interface JsonSerializable oblige à définir la méthode
+     *  JsonSerialize et permet à json_encode de savoir comment convertir un Category en JSON.
      * 
      *  Parenthèse sur les "interfaces" : Une interface est une classe (abstraite en générale) qui
      *  regroupe un ensemble de méthodes. On dit que "une classe implémente une interface" au lieu de dire 
@@ -47,36 +51,40 @@ class Category implements JsonSerializable {
      *  
      */
     public function JsonSerialize(): mixed{
-        return ["id" => $this->id, "name" => $this->name];
+        return ["id_category" => $this->id_category, "libelle_cate" => $this->libelle_cate];
     }
 
     /**
-     * Get the value of name
+     * Get the value of libelle
      */ 
-    public function getName()
+    public function getlibelleCate()
     {
-        return $this->name;
+        return $this->libelle_cate;
     }
 
     /**
-     * Set the value of name
+     * Set the value of libelle
      *
      * @return  self
      */ 
-    public function setName($name): self
+    public function setlibelleCate($libelle_cate): self
     {
-        $this->name = $name;
+        $this->libelle_cate = $libelle_cate;
         return $this;
     }
+
+
 
     /**
      * Set the value of id
      *
      * @return  self
      */ 
-    public function setId($id): self
+    public function setIdCategory($id_category): self
     {
-        $this->id = $id;
+        $this->id_category = $id_category;
         return $this;
     }
+    
+    
 }
