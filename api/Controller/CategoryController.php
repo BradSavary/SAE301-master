879bee96d098ebc19e7a494ef3eb48c17c4bdec2
GilianC <163392501+GilianC@users.noreply.github.com>
2024-10-21 10:27:26 +0200

    Update CategoryController.php

--- api/Controller/CategoryController.php
+++ api/Controller/CategoryController.php
@@ -8,28 +8,38 @@ require_once "Repository/CategoryRepository.php" ;
 
 class CategoryController extends Controller {
 
-    private CategoryRepository $category;
+    private categoryRepository $categorys;
 
     public function __construct(){
-        $this->category = new CategoryRepository();
+        $this->categorys = new categoryRepository();
     }
 
    
     protected function processGetRequest(HttpRequest $request) {
         $id = $request->getId("id");
         if ($id){
-            // URI is .../category/{id}
-            $p = $this->category->find($id);
+            // URI is .../categorys/{id}
+            $p = $this->categorys->find($id);
             return $p==null ? false :  $p;
         }
         else{
-        return $this->category->findAll();
-
+            // URI is .../categorys
+            $cat = $request->getParam("category"); // is there a category parameter in the request ?
+            if ( $cat == false) // no request category, return all categorys
+                return $this->categorys->findAll();
+            else // return only categorys of category $cat
+                return $this->categorys->findAllByCategory($cat);
         }
     }
 
     protected function processPostRequest(HttpRequest $request) {
-        return false;
+        $json = $request->getJson();
+        $obj = json_decode($json);
+        $p = new category(0); // 0 is a symbolic and temporary value since the product does not have a real id yet.
+        $p->setlibelleCate($obj->libelle_cate);
+
+        $ok = $this->categorys->save($p); 
+        return $ok ? $p : false;
     }
    
 }
